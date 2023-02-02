49368265ff901e4507e635036394a81b75f40a0c
julia leal <julialeal.t@gmail.com>
2023-02-02 12:58:59 -0300

    estruturas de repeticao em js

---
+++ CursoJS/aula 13/ambiente.js
@@ -0,0 +1,27 @@
+
+// Estrutura de repetição com teste lógico no final
+var cont = 1 
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 5)
+
+
+
+
+// Estrutura de repetição com teste lógico no início
+var cont = 1 
+while (cont <= 5) {
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+
+
+/*
+console.log("Tudo bem? ")
+console.log("Tudo bem? ")
+console.log("Tudo bem? ")
+console.log("Tudo bem? ")
+console.log("Tudo bem? ")
+*/
---
+++ CursoJS/aula 14/ambiente.js
@@ -0,0 +1,9 @@
+// Estrutura de repetição com variável de controle
+
+console.log('Vai começar...')
+
+for (var i = 1; i <= 4; i++) {
+    console.log(i)
+}
+
+console.log('FIM')
