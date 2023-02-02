b68a98bd541f208d7bf8af9bfc77c14d40a7c456
julia leal <julialeal.t@gmail.com>
2023-02-02 16:46:20 -0300

    site tabuada

--- CursoJS/exerciciospt2/ex017/script.js
+++ CursoJS/exerciciospt2/ex017/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function gerarTabuada() {
+    var tnum = window.document.getElementById('txtnum')
+    var tab = window.document.getElementById('seltab')
+
+
+    if (tnum.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }
+    else{
+
+        var num = Number(tnum.value)
+        var mult = 1
+        tab.innerHTML = ''
+
+        for (let i = 1; i <= 10; i++) {
+            mult = num * i
+            var item = window.document.createElement('option')
+            item.text = `${num} x ${i} = ${mult}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }    
+}
